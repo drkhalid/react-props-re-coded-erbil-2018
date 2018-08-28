267eda999698d91f0955a6a579fc7b9871a0784b
khalid jamal <kh.jamal95@gmail.com>
2018-08-28 23:23:43 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 ReactDOM.render(
   <MovieCard title="Mad Max: Fury Road" poster="http://image.tmdb.org/t/p/w342/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" genres={['Action', 'Adventure', 'Science Fiction', 'Thriller']} />,
   document.getElementById('root')
