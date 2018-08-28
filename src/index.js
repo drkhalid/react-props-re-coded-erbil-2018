4b0e0e98c06f0e1b0250b21d6ccfd7142281b06f
khalid jamal <kh.jamal95@gmail.com>
2018-08-28 23:24:14 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+const madMaxGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+ 
 ReactDOM.render(
   <MovieCard 
     title="Mad Max: Fury Road"
     poster="http://image.tmdb.org/t/p/w342/kqjL17yufvn9OVLyXYpvtyrFfak.jpg"
-    genres={['Action', 'Adventure', 'Science Fiction', 'Thriller']}
+    genres={madMaxGenres}
   />,
   document.getElementById('root')
 );
